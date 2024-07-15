97792a642c074989cd19db4c07f391bc666fa2c0
Ariel Freitas <94645030+ArielFSantos@users.noreply.github.com>
2024-07-15 15:47:29 -0300

    json
    insert package json and init of create header

---
+++ ariel-freitas/src/header/header.js
@@ -0,0 +1,15 @@
+function Header(){
+    return(
+    <>
+      <h1>Teste</h1>
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Jobs</li>
+            </ul>
+        </nav>
+    </>
+    )
+}
+export default Header
